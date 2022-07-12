8ba8c3237dc448c4bd5ff13abffa025d76c5421a
RPE <vitali.shuman96@gmail.com>
2022-07-12 13:33:07 +0300

    Add declaration of possible selections and their props
    Add array of possible selection that contains text to be displayed on UI, id and array of other selection that current entity can beat.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//
+const AVAILABLE_SELECTIONS = [
+  {
+    id: "PAPER",
+    name: "Papper",
+    beats: ["ROCK"],
+  },
+  {
+    id: "ROCK",
+    name: "Rock",
+    beats: ["SCISSORS"],
+  },
+  {
+    id: "SCISSORS",
+    name: "Scissors",
+    beats: "PAPER",
+  },
+];
